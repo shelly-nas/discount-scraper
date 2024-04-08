99da37ee54f1623b5d43ee160f9dd6d8b1d03011
Jelle <jellevandergraaf@live.com>
2024-04-08 12:12:12 +0200

    Add JSON property validation

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,25 @@ import ArgumentHandler from './utils/ArgumentHandler';
 import JsonReader from './utils/JsonReader';
 import process from 'process';
 
-function getConfig(): any {
+async function getConfig(): Promise<Grocery> {
     const argHandler = new ArgumentHandler(process.argv);
     const configPath = argHandler.getArgByFlag('--config');
 
     const reader = new JsonReader(configPath);
-    const jsonData = reader.read();
+    const jsonData = await reader.read();
     
     logger.info('JSON data read from file:', jsonData);
     return jsonData
 }
 
 async function main() {
-    const jsonData = getConfig();
+    const jsonData = await getConfig();
 
     const webClient = new WebClient();
 
     await webClient.init();
     await webClient.navigate(jsonData.url);
     await webClient.handleCookiePopup([jsonData.cookieDecline])
-    
-    const title = await webClient.getTitle();
-    logger.info(`The title of the page is: ${title}`);
 
     await webClient.close();
 }
--- src/utils/JsonReader.ts
+++ src/utils/JsonReader.ts
@@ -1,23 +1,56 @@
 import { logger } from './Logger';
+import Ajv from 'ajv';
 import fs from 'fs';
 import path from 'path';
 
 class JsonReader {
-    private filePath: string;
+    private schemaPath: string = 'config/schema.json';
+    private groceryPath: string;
 
     constructor(filePath: string) {
-        this.filePath = filePath;
+        this.groceryPath = filePath;
     }
 
-    read(): any {
+    private createJsonObject(filePath: string): any {
         try {
             // Ensure the file path is absolute
-            const absolutePath = path.resolve(this.filePath);
-            const fileContent = fs.readFileSync(absolutePath, 'utf8');
+            const absolutePath: string = path.resolve(filePath);
+            const fileContent: string = fs.readFileSync(absolutePath, 'utf8');
             return JSON.parse(fileContent);
         } catch (error) {
-            logger.error(`Failed to read or parse the JSON file at ${this.filePath}:`, error);
-            throw error;
+            logger.error(`Failed to read or parse the JSON file at ${this.groceryPath}:`, error);
+            process.exit(0);
+        }
+    }   
+
+    private async validateObject(jsonData: object): Promise<{ isValid: boolean; errors: any[] | null | undefined }> {
+        const parsedSchema: Grocery = this.createJsonObject(this.schemaPath);
+        
+        // Compile the schema
+        const ajv = new Ajv({allErrors: true});
+        const schemaObject = ajv.compile(parsedSchema);
+        
+        // Validate the data
+        const isValid = await schemaObject(jsonData);
+        
+        return {
+            isValid,
+            errors: schemaObject.errors // This will be `null` if data is valid
+        };
+    }
+
+    public async read(): Promise<Grocery> {         
+        const parsedGrocery: Grocery = this.createJsonObject(this.groceryPath);
+        
+        const {isValid, errors} = await this.validateObject(parsedGrocery)
+        
+        if (!isValid) {
+            const errorMissingProperty = errors?.map(error => error.params.missingProperty).join(', ');
+            logger.error(`JSON key validation failed. Properties missing: '${errorMissingProperty}'`)
+            process.exit(0);
+        }
+        else {
+            return parsedGrocery
         }
     }
 }
