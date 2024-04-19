5497d1ef5679dacbcadf07781eb2ae3adb9d33c0
Jelle <jellevandergraaf@live.com>
2024-04-19 14:09:48 +0200

    Format helpers

--- src/utils/helpers/DateTimeHandler.ts
+++ src/utils/helpers/DateTimeHandler.ts
@@ -1,34 +1,35 @@
 export default class DateTimeHandler {
-    
-    private static getFormattedDateTime(): {
-        year: string,
-        month: string,
-        day: string,
-        hours: string,
-        minutes: string,
-        seconds: string,
-        milliseconds: string
-    } {
-        const now = new Date();
+  private static getFormattedDateTime(): {
+    year: string;
+    month: string;
+    day: string;
+    hours: string;
+    minutes: string;
+    seconds: string;
+    milliseconds: string;
+  } {
+    const now = new Date();
 
-        return {
-            year: now.getUTCFullYear().toString(),
-            month: (now.getUTCMonth() + 1).toString().padStart(2, '0'),
-            day: now.getUTCDate().toString().padStart(2, '0'),
-            hours: now.getUTCHours().toString().padStart(2, '0'),
-            minutes: now.getUTCMinutes().toString().padStart(2, '0'),
-            seconds: now.getUTCSeconds().toString().padStart(2, '0'),
-            milliseconds: now.getUTCMilliseconds().toString().padStart(3, '0')
-        };
-    }
+    return {
+      year: now.getUTCFullYear().toString(),
+      month: (now.getUTCMonth() + 1).toString().padStart(2, "0"),
+      day: now.getUTCDate().toString().padStart(2, "0"),
+      hours: now.getUTCHours().toString().padStart(2, "0"),
+      minutes: now.getUTCMinutes().toString().padStart(2, "0"),
+      seconds: now.getUTCSeconds().toString().padStart(2, "0"),
+      milliseconds: now.getUTCMilliseconds().toString().padStart(3, "0"),
+    };
+  }
 
-    public static getDateTimeLong(): string {
-        const {year, month, day, hours, minutes, seconds, milliseconds} = this.getFormattedDateTime();
-        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
-    }
+  public static getDateTimeLong(): string {
+    const { year, month, day, hours, minutes, seconds, milliseconds } =
+      this.getFormattedDateTime();
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+  }
 
-    public static getDateTimeShort(): string {
-        const {year, month, day, hours, minutes, seconds} = this.getFormattedDateTime();
-        return `${year}${month}${day}-${hours}${minutes}${seconds}`;
-    }
+  public static getDateTimeShort(): string {
+    const { year, month, day, hours, minutes, seconds } =
+      this.getFormattedDateTime();
+    return `${year}${month}${day}-${hours}${minutes}${seconds}`;
+  }
 }
--- src/utils/helpers/Logger.ts
+++ src/utils/helpers/Logger.ts
@@ -1,92 +1,97 @@
-import DateTimeHandler from './DateTimeHandler';
-import fs from 'fs';
-import path from 'path';
+import DateTimeHandler from "./DateTimeHandler";
+import fs from "fs";
+import path from "path";
 
 enum LogLevel {
-    ERROR = 1,
-    WARN = 2,
-    INFO = 3,
-    DEBUG = 4
+  ERROR = 1,
+  WARN = 2,
+  INFO = 3,
+  DEBUG = 4,
 }
 
 class Logger {
-    private static instance: Logger;
-    private readonly logFilePath: string;
-    private readonly logDirectory: string = './log';
-    private currentLogLevel: LogLevel = LogLevel.INFO; // Default to INFO
+  private static instance: Logger;
+  private readonly logFilePath: string;
+  private readonly logDirectory: string = "./log";
+  private currentLogLevel: LogLevel = LogLevel.INFO; // Default to INFO
 
-    private constructor() {
-        if (!fs.existsSync(this.logDirectory)) {
-            fs.mkdirSync(this.logDirectory, { recursive: true });
+  private constructor() {
+    if (!fs.existsSync(this.logDirectory)) {
+      fs.mkdirSync(this.logDirectory, { recursive: true });
+    }
+
+    const levelName =
+      (process.env.LOG_LEVEL as keyof typeof LogLevel) || "INFO";
+    this.setLogLevel(LogLevel[levelName]);
+
+    const logFileName = `discountScraper_${DateTimeHandler.getDateTimeShort()}.log`;
+    this.logFilePath = path.join(this.logDirectory, logFileName);
+  }
+
+  public static getInstance(): Logger {
+    if (!Logger.instance) {
+      Logger.instance = new Logger();
+    }
+    return Logger.instance;
+  }
+
+  private setLogLevel(level: LogLevel) {
+    this.currentLogLevel = level;
+  }
+
+  private shouldLog(level: LogLevel): boolean {
+    return level <= this.currentLogLevel;
+  }
+
+  private writeToFile(
+    level: string,
+    message: string,
+    ...optionalParams: any[]
+  ): void {
+    if (!this.shouldLog(LogLevel[level as keyof typeof LogLevel])) {
+      return;
+    }
+
+    // Compose the initial part of the log message.
+    const logMessage = `[${DateTimeHandler.getDateTimeLong()}] [${level}] ${message}`;
+
+    try {
+      // Append the initial message to the file.
+      fs.appendFileSync(this.logFilePath, logMessage);
+
+      // Append each optional parameter as a new entry in the log file.
+      optionalParams.forEach((param) => {
+        let paramAsString;
+        if (typeof param === "object") {
+          paramAsString = JSON.stringify(param, null, 2); // Prettify objects.
+        } else {
+          paramAsString = String(param); // Convert others to string.
         }
+        fs.appendFileSync(this.logFilePath, " " + paramAsString);
+      });
 
-        const levelName = process.env.LOG_LEVEL as keyof typeof LogLevel || 'INFO';
-        this.setLogLevel(LogLevel[levelName]);
-
-        const logFileName = `discountScraper_${DateTimeHandler.getDateTimeShort()}.log`;
-        this.logFilePath = path.join(this.logDirectory, logFileName);
-    } 
-
-    public static getInstance(): Logger {
-        if (!Logger.instance) {
-            Logger.instance = new Logger();
-        }
-        return Logger.instance;
+      // Add a newline at the end of the log entry.
+      fs.appendFileSync(this.logFilePath, "\n");
+    } catch (error) {
+      console.error("Error writing to log file:", error);
     }
+  }
 
-    private setLogLevel(level: LogLevel) {
-        this.currentLogLevel = level;
-    }
+  public debug(message: string, ...optionalParams: any[]) {
+    this.writeToFile("DEBUG", message, ...optionalParams);
+  }
 
-    private shouldLog(level: LogLevel): boolean {
-        return level <= this.currentLogLevel;
-    }
-    
-    private writeToFile(level: string, message: string, ...optionalParams: any[]): void {
-        if (!this.shouldLog(LogLevel[level as keyof typeof LogLevel])) {
-            return;
-        }
+  public info(message: string, ...optionalParams: any[]) {
+    this.writeToFile("INFO", message, ...optionalParams);
+  }
 
-        // Compose the initial part of the log message.
-        const logMessage = `[${DateTimeHandler.getDateTimeLong()}] [${level}] ${message}`;
+  public warn(message: string, ...optionalParams: any[]) {
+    this.writeToFile("WARN", message, ...optionalParams);
+  }
 
-        try {
-            // Append the initial message to the file.
-            fs.appendFileSync(this.logFilePath, logMessage);
-
-            // Append each optional parameter as a new entry in the log file.
-            optionalParams.forEach((param) => {
-                let paramAsString;
-                if (typeof param === 'object') {
-                    paramAsString = JSON.stringify(param, null, 2); // Prettify objects.
-                } else {
-                    paramAsString = String(param); // Convert others to string.
-                }
-                fs.appendFileSync(this.logFilePath, ' ' + paramAsString);
-            });
-
-            // Add a newline at the end of the log entry.
-            fs.appendFileSync(this.logFilePath, '\n');
-        } catch (error) {
-            console.error('Error writing to log file:', error);
-        }
-    }
-    
-    public debug(message: string, ...optionalParams: any[]) {
-        this.writeToFile('DEBUG', message, ...optionalParams);
-    }
-
-    public info(message: string, ...optionalParams: any[]) {
-        this.writeToFile('INFO', message, ...optionalParams);
-    }
-
-    public warn(message: string, ...optionalParams: any[]) {
-        this.writeToFile('WARN', message, ...optionalParams);
-    }
-
-    public error(message: string, ...optionalParams: any[]) {
-        this.writeToFile('ERROR', message, ...optionalParams);
-    }
+  public error(message: string, ...optionalParams: any[]) {
+    this.writeToFile("ERROR", message, ...optionalParams);
+  }
 }
 
 export const logger = Logger.getInstance();
