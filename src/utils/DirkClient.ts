563b9ffeb3ae009fd2d122457ebe7fbdb854edc0
Jelle <jellevandergraaf@live.com>
2024-04-19 14:13:27 +0200

    Format utils

--- src/utils/DirkClient.ts
+++ src/utils/DirkClient.ts
@@ -3,45 +3,68 @@ import GroceryClient from "./GroceryClient";
 import { logger } from "./helpers/Logger";
 
 class DirkClient extends GroceryClient {
+  constructor() {
+    super();
+    logger.info("Created a Dirk Grocery Client instance.");
+  }
 
-    constructor() {
-        super();
-        logger.info('Created a Dirk Grocery Client instance.')
+  public async getOriginalPrice(
+    anchorHandle: ElementHandle,
+    originalPriceSelector: string[]
+  ): Promise<string> {
+    try {
+      // Directly retrieve the nested content if the structure and access pattern are consistent and predictable
+      const price = await anchorHandle.$$eval(
+        originalPriceSelector[0],
+        (elements, selector) => {
+          return elements
+            .map((element) => {
+              const span = element.querySelector(selector);
+              return span ? span.textContent : "";
+            })
+            .join("");
+        },
+        originalPriceSelector[1]
+      ); // Passing the second selector part as an argument to the page function
+      logger.debug(`Original price retrieved: '${price}'.`);
+      return price.trim();
+    } catch (error) {
+      logger.warn(
+        `Warn retrieving original price with selector '${originalPriceSelector.join(
+          " > "
+        )}':`,
+        error
+      );
+      return "";
     }
+  }
 
-    public async getOriginalPrice(anchorHandle: ElementHandle, originalPriceSelector: string[]): Promise<string> {
-        try {            
-            // Directly retrieve the nested content if the structure and access pattern are consistent and predictable
-            const price = await anchorHandle.$$eval(originalPriceSelector[0], (elements, selector) => {
-                return elements.map(element => {
-                    const span = element.querySelector(selector);
-                    return span ? span.textContent : '';
-                }).join('');
-            }, originalPriceSelector[1]); // Passing the second selector part as an argument to the page function
-            logger.debug(`Original price retrieved: '${price}'.`);
-            return price.trim();
-        } catch (error) {
-            logger.warn(`Warn retrieving original price with selector '${originalPriceSelector.join(' > ')}':`, error);
-            return '';
-        }
+  public async getDiscountPrice(
+    anchorHandle: ElementHandle,
+    discountPriceSelector: string[]
+  ): Promise<string> {
+    try {
+      // Concatenate the euro and cent values directly within a single evaluate to minimize calls to the browser context
+      const price = await anchorHandle.evaluate((node: Element, selectors) => {
+        const euros = node.querySelector(selectors[0])?.textContent || "0";
+        let cents =
+          node.querySelector(selectors[1])?.textContent ||
+          node.querySelector(selectors[2])?.textContent ||
+          "00";
+        return `${euros}.${cents}`;
+      }, discountPriceSelector);
+      logger.debug(`Discount price retrieved: '${price}'.`);
+      return price.trim();
+    } catch (error) {
+      logger.warn(
+        `Warn retrieving discount price with selector '${discountPriceSelector.join(
+          ", "
+        )}':`,
+        error
+      );
+      return "";
     }
-    
-    public async getDiscountPrice(anchorHandle: ElementHandle, discountPriceSelector: string[]): Promise<string> {
-        try {
-            // Concatenate the euro and cent values directly within a single evaluate to minimize calls to the browser context
-            const price = await anchorHandle.evaluate((node: Element, selectors) => {
-                const euros = node.querySelector(selectors[0])?.textContent || '0';
-                let cents = node.querySelector(selectors[1])?.textContent ||
-                            node.querySelector(selectors[2])?.textContent || '00';
-                return `${euros}.${cents}`;
-            }, discountPriceSelector);
-            logger.debug(`Discount price retrieved: '${price}'.`);
-            return price.trim();
-        } catch (error) {
-            logger.warn(`Warn retrieving discount price with selector '${discountPriceSelector.join(', ')}':`, error);
-            return '';
-        }
-    }
+  }
 }
 
 export default DirkClient;
--- src/utils/WebClient.ts
+++ src/utils/WebClient.ts
@@ -1,47 +1,53 @@
-import { chromium, Browser, Page } from 'playwright';
-import { logger } from './helpers/Logger';
+import { chromium, Browser, Page } from "playwright";
+import { logger } from "./helpers/Logger";
 
 class WebClient {
-    private browser: Browser | null = null;
-    protected page: Page | null = null;
+  private browser: Browser | null = null;
+  protected page: Page | null = null;
 
-    public async init(): Promise<void> {
-        try {
-            logger.debug('Initializing browser...');
-            this.browser = await chromium.launch({ headless: false, slowMo: 50 });
-            this.page = await this.browser.newPage();
-            logger.info('Browser initialized successfully.');
-        } catch (error) {
-            logger.error('Browser initialized unsuccessfully.', error)
-            process.exit(1);
-        }
+  public async init(): Promise<void> {
+    try {
+      logger.debug("Initializing browser...");
+      this.browser = await chromium.launch({ headless: false, slowMo: 50 });
+      this.page = await this.browser.newPage();
+      logger.info("Browser initialized successfully.");
+    } catch (error) {
+      logger.error("Browser initialized unsuccessfully.", error);
+      process.exit(1);
     }
+  }
 
-    public async navigate(url: string): Promise<void> {
-        logger.info(`Navigating to URL: ${url}`);
-        await this.page?.goto(url);
-    }
+  public async navigate(url: string): Promise<void> {
+    logger.info(`Navigating to URL: ${url}`);
+    await this.page?.goto(url);
+  }
 
-    public async handleCookiePopup(selector: string): Promise<void> {
-        if (selector) {
-            try {
-                await this.page?.waitForSelector(selector, { state: "visible", timeout: 5000 });
-                logger.debug(`Found cookie popup with selector '${selector}'.`);
-                await this.page?.click(selector);
-                logger.info('Dismissed cookie popup');
-            } catch (error) {
-                logger.error(`No cookie popup found with selector: ${selector} or timeout exceeded.`, error);
-            }
-        } else {
-            logger.info('No cookie popup selector provided to dismiss.');
-        }
+  public async handleCookiePopup(selector: string): Promise<void> {
+    if (selector) {
+      try {
+        await this.page?.waitForSelector(selector, {
+          state: "visible",
+          timeout: 5000,
+        });
+        logger.debug(`Found cookie popup with selector '${selector}'.`);
+        await this.page?.click(selector);
+        logger.info("Dismissed cookie popup");
+      } catch (error) {
+        logger.error(
+          `No cookie popup found with selector: ${selector} or timeout exceeded.`,
+          error
+        );
+      }
+    } else {
+      logger.info("No cookie popup selector provided to dismiss.");
     }
-    
-    async close(): Promise<void> {
-        logger.debug('Closing browser...');
-        await this.browser?.close();
-        logger.info('Browser closed.');
-    }
+  }
+
+  async close(): Promise<void> {
+    logger.debug("Closing browser...");
+    await this.browser?.close();
+    logger.info("Browser closed.");
+  }
 }
 
 export default WebClient;
