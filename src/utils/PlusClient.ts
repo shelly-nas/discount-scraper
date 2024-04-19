ef1b384cf91b2ac3a7d756cd2ce6c25be98c62e9
Jelle <jellevandergraaf@live.com>
2024-04-19 14:05:40 +0200

    Update how to receive the productName

--- src/utils/PlusClient.ts
+++ src/utils/PlusClient.ts
@@ -9,20 +9,10 @@ class PlusClient extends GroceryClient {
         logger.info('Created a PLUS Grocery Client instance.')
     }
 
-    protected async getProductName(anchorHandle: ElementHandle, productNameSelector: string[]): Promise<string> {
-        try {
-            const productName = await anchorHandle.getAttribute(productNameSelector[0]) || '';
-            logger.debug(`Product name retrieved: '${productName}'.`);
-            return productName.trim();
-        } catch (error) {
-            logger.error(`Error retrieving product name with selector '${productNameSelector[0]}':`, error);
-            return '';
-        }
-    }
-
     public async getOriginalPrice(anchorHandle: ElementHandle, originalPriceSelector: string[]): Promise<string> {
         try {
             const price = await anchorHandle.$eval(originalPriceSelector[0], span => span.textContent);
+
             logger.debug(`Original price retrieved: '${price}'.`);
             return price !== null ? price.trim() : '';
         } catch (error) {
@@ -33,18 +23,12 @@ class PlusClient extends GroceryClient {
 
     public async getDiscountPrice(anchorHandle: ElementHandle, discountPriceSelector: string[]): Promise<string> {
         try {
-            // const price = await anchorHandle.evaluate((node: Element, selectors) => {
-            //     const euros = node.querySelector(selectors[0])?.textContent || '0';
-            //     let cents = node.querySelector(selectors[1])?.textContent ||
-            //                 node.querySelector(selectors[2])?.textContent || '00';
-            //     return `${euros}.${cents}`;
-            // }, discountPriceSelector);
-
             const price = await anchorHandle.evaluate((node: Element, selectors: string[]) => {
                 const euros = node.querySelector(selectors[0])?.textContent || '';
                 const cents = node.querySelector(selectors[1])?.textContent || '';
                 return `${euros}${cents}`;
             }, discountPriceSelector);
+            
             logger.debug(`Discount price retrieved: '${price}'.`);
             return price !== null ? price.trim() : '';
         } catch (error) {
