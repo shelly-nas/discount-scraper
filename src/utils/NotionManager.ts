f82766af8b72b1dee3c3ae99cdcf44feeff17c9a
Jelle <jellevandergraaf@live.com>
2024-04-26 15:23:19 +0200

    Enable NotionBlocks

--- src/utils/NotionManager.ts
+++ src/utils/NotionManager.ts
@@ -22,42 +22,42 @@ export default class NotionManager {
     };
   }
 
-  // public toPageBlocks(groceryDiscounts: IGroceryDiscount): IBlock[] {
-  //   try {
-  //     this.addHeading3(groceryDiscounts.groceryName);
-  //     groceryDiscounts.discounts.forEach((discount) => {
-  //       this.addTodo(
-  //         `${discount.productName}: ${discount.discountPrice} (${discount.specialDiscount})`
-  //       );
-  //     });
-  //     this.addDivider();
+  public toPageBlocks(groceryDiscounts: IGroceryDiscount[]): IBlock[] {
+    try {
+      this.addHeading3(groceryDiscounts[0].supermarket);
+      groceryDiscounts.forEach((discount) => {
+        this.addTodo(
+          `${discount.productName}: ${discount.discountPrice} (${discount.specialDiscount})`
+        );
+      });
+      this.addDivider();
 
-  //     logger.info(
-  //       `Converted grocery discounts for '${groceryDiscounts.groceryName}' to Notion page blocks.`
-  //     );
-  //     return this.blocks;
-  //   } catch (error) {
-  //     logger.error(
-  //       `Error converting grocery discounts for '${groceryDiscounts.groceryName}' to Notion page blocks: ${error}`
-  //     );
-  //     // Handling error appropriately, here we simply throw it to be dealt by the caller
-  //     throw error;
-  //   }
-  // }
+      logger.info(
+        `Converted grocery discounts for '${groceryDiscounts[0].supermarket}' to Notion page blocks.`
+      );
+      return this.blocks;
+    } catch (error) {
+      logger.error(
+        `Error converting grocery discounts for '${groceryDiscounts[0].supermarket}' to Notion page blocks: ${error}`
+      );
+      // Handling error appropriately, here we simply throw it to be dealt by the caller
+      throw error;
+    }
+  }
 
-  // private addHeading3(richText: string) {
-  //   const newHeading = new Heading3([new RichText(richText)]);
-  //   this.blocks.push(newHeading);
-  // }
+  private addHeading3(richText: string) {
+    const newHeading = new Heading3([new RichText(richText)]);
+    this.blocks.push(newHeading);
+  }
 
-  // private addTodo(richText: string) {
-  //   const newTodo = new Todo([new RichText(richText)]);
-  //   this.blocks.push(newTodo);
-  // }
+  private addTodo(richText: string) {
+    const newTodo = new Todo([new RichText(richText)]);
+    this.blocks.push(newTodo);
+  }
 
-  // private addDivider() {
-  //   this.blocks.push(new Divider());
-  // }
+  private addDivider() {
+    this.blocks.push(new Divider());
+  }
 
   public toDatabaseEntries(
     discounts: IGroceryDiscount[]
