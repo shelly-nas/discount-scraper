d71a91b987cdcd8440ed4b2e4e252212f3bc2ad7
Jelle <jellevandergraaf@live.com>
2024-04-26 09:24:02 +0200

    AI integration with OpenAi created

---
+++ src/clients/ai/OpenAIClient.ts
@@ -0,0 +1,52 @@
+import OpenAI from "openai";
+import { logger } from "../../utils/Logger";
+
+class OpenAIClient {
+  private openAI: OpenAI;
+  private completion: any;
+  private maxTokens: number = 4096; 
+
+  constructor(apiKey: string) {
+    this.openAI = new OpenAI({ apiKey: apiKey });
+    logger.info("OpenAI client authenticated.");
+  }
+
+  public getCompletionContent(): string{
+    return this.completion.choices[0].message.content;
+  }
+
+  public async categorizeProducts(discountProducts: string, productCategories: string): Promise<void> {
+    const onlyJson = "Only respond with the completed JSON, without a code block wrapper. ";
+    const inputJson = `The JSON structure to complete: '${discountProducts}'. `;
+    const infoJson = `The ProductCategories JSON structure: '${productCategories}'. `;
+    const prompt = "Match the products with a single product category. For each 'productName' fill the 'category' key, with the 'id' key of corresponding ProductCategories. ";
+    const customizedPrompt = prompt + inputJson + infoJson + onlyJson;
+    logger.debug("AI Request:", customizedPrompt)
+
+    await this
+      .sendPrompt(customizedPrompt)
+      .catch((error) => {
+        logger.error("Failed to get response:", error);
+      });
+  }
+
+  private async sendPrompt(prompt: string): Promise<void> {
+    const completion = await this.openAI.chat.completions.create({
+      messages: [
+        { role: "system", content: prompt },
+      ],
+      model: "gpt-4-turbo-preview",
+      max_tokens: this.maxTokens,
+      temperature: 0,
+      // response_format: { 
+      //   type: "json_object",
+      // },
+    });
+    logger.info("Categorize product into custom set of product categories.");
+    logger.debug("AI Response:", completion);
+    
+    this.completion = completion;
+  }
+}
+
+export default OpenAIClient;
