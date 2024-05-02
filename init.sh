127c8a70ecf9cf331e94f2cd06d41c3ccea644bd
Jelle <jellevandergraaf@live.com>
2024-05-02 14:14:26 +0200

    Create .evn file in init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,13 @@ npm install
 
 # Compile TypeScript
 npm run build
+
+# Create .env file
+cat <<EOL >> .env
+LOG_LEVEL=INFO
+DB_PRODUCT=./database/Product.json
+DB_DISCOUNT=./database/Discount.json
+SUPERMARKET_WEB_CONFIG_SCHEMA=config/supermarketWebConfigSchema.json
+NOTION_SECRET=${NOTION_SECRET}
+NOTION_DATABASE_ID=${NOTION_DATABASE_ID}
+EOL
