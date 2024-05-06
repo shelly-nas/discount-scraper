7b490646266e6f7275bb55d017b3b81eed84f326
Jelle <jellevandergraaf@live.com>
2024-05-06 13:18:59 +0200

    env file to created properly

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Install dependencies
 npm install
 
@@ -10,12 +11,17 @@ npm run build
 # Remove the .env file if it exists
 sudo rm -f .env
 
-# Create .env file
-cat <<EOL >> .env
-LOG_LEVEL=$LOG_LEVEL
-DB_PRODUCT=$DB_PRODUCT
-DB_DISCOUNT=$DB_DISCOUNT
-SUPERMARKET_WEB_CONFIG_SCHEMA=$SUPERMARKET_WEB_CONFIG_SCHEMA
-NOTION_SECRET=$NOTION_SECRET
-NOTION_DATABASE_ID=$NOTION_DATABASE_ID
-EOL
+# Define the path to the .env file
+ENV_FILE=".env"
+
+# Write the environment variables to the .env file
+{
+    echo "LOG_LEVEL=${LOG_LEVEL}"
+    echo "DB_PRODUCT=${DB_PRODUCT}"
+    echo "DB_DISCOUNT=${DB_DISCOUNT}"
+    echo "SUPERMARKET_WEB_CONFIG_SCHEMA=${SUPERMARKET_WEB_CONFIG_SCHEMA}"
+    echo "NOTION_SECRET=${NOTION_SECRET}"
+    echo "NOTION_DATABASE_ID=${NOTION_DATABASE_ID}"
+} > $ENV_FILE
+
+echo ".env file generated successfully!"
